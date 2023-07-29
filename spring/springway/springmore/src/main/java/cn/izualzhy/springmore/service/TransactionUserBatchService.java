9660953da14bd5e686c50f795fd00ead59c01a7b
izualzhy <izualzhy@163.com>
2023-07-29 17:02:20 +0800

    Revert "update"
    This reverts commit ca9e2f559da5fc0ec2e0f608099b061e6debed46.

--- spring/springway/springmore/src/main/java/cn/izualzhy/springmore/service/TransactionUserBatchService.java
+++
@@ -1,9 +0,0 @@
-package cn.izualzhy.springmore.service;
-
-import cn.izualzhy.springmore.pojo.TransactionUser;
-
-import java.util.List;
-
-public interface TransactionUserBatchService {
-    public int insertUsers(List<TransactionUser> transactionUserList);
-}
