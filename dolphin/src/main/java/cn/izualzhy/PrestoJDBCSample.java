f68d3d52681b338554b54e406a8f720c38261681
izualzhy <izualzhy@163.com>
2023-10-28 21:29:13 +0800

    update

---
+++ dolphin/src/main/java/cn/izualzhy/PrestoJDBCSample.java
@@ -0,0 +1,55 @@
+package cn.izualzhy;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class PrestoJDBCSample {
+    static HikariDataSource hikariDataSource;
+    static Connection initConnection(String connectionUrl) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "test");
+
+        Connection connection = DriverManager.getConnection(connectionUrl, properties);
+        return connection;
+    }
+
+    static void initHikariConnection(String connectionUrl) {
+        hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName("com.facebook.presto.jdbc.PrestoDriver");
+        hikariDataSource.setJdbcUrl(connectionUrl);
+        hikariDataSource.addDataSourceProperty("user", "test");
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+//        test(args);
+        testHikari(args);
+    }
+
+    private static void test(String[] args) throws SQLException {
+        try (Connection connection = initConnection(args[0])){
+            try (Statement statement = connection.createStatement()){
+                try (ResultSet resultSet = statement.executeQuery("SELECT 1")) {
+                    while (resultSet.next()) {
+                        System.out.println("result = " + resultSet.getInt(1));
+                    }
+                }
+            }
+        }
+    }
+
+    private static void testHikari(String[] args) throws SQLException {
+        initHikariConnection(args[0]);
+        try (Connection connection = (hikariDataSource.getConnection())){
+            try (Statement statement = connection.createStatement()){
+                try (ResultSet resultSet = statement.executeQuery("SELECT 1")) {
+                    while (resultSet.next()) {
+                        System.out.println("result = " + resultSet.getInt(1));
+                    }
+                }
+            }
+        }
+    }
+}
