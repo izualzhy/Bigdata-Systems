98513dd3e5d6338175733161228523886258367c
izualzhy <izualzhy@163.com>
2024-02-03 20:12:25 +0800

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+项目记录我在大数据的一些实践代码，相关文章分享在[izualzhy.cn](https://izualzhy.cn/)
+
---
+++ flink/flink_1_12/src/main/scala/cn/izualzhy/ATestStream.scala
@@ -0,0 +1,17 @@
+package cn.izualzhy
+
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+
+object ATestStream extends App {
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  private val threshold = 5
+
+  env.fromSequence(1, 10)
+    .filter(_ > threshold)
+    .print("result ")
+
+  env.execute()
+}
